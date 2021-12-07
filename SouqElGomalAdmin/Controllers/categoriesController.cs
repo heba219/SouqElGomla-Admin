fbb9831404a956f1b74fc35c6dfdd61aae644552
Heba <hebayousry219@gmail.com>
2021-12-07 05:36:19 +0200

    ui done

---
+++ SouqElGomalAdmin/Controllers/categoriesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SouqElGomalAdmin.Controllers
+{
+    public class categoriesController : Controller
+    {
+        // GET: categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SouqElGomalAdmin/Controllers/usersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SouqElGomalAdmin.Controllers
+{
+    public class usersController : Controller
+    {
+        // GET: users
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
